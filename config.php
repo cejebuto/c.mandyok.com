7d3b450e1b4299619fe134506537deb756687bab
cejebuto <cejebuto@gmail.com>
2012-10-11 17:58:56 -0500

    Inicio de Proyecto

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * The base MySQL settings of OSClass
+ */
+define('MULTISITE', 0);
+
+/** MySQL database name for OSClass */
+define('DB_NAME', 'tienda');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'geistover');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Table prefix */
+define('DB_TABLE_PREFIX', 'tbl_');
+
+define('REL_WEB_URL', '/t.mandyok.com/');
+
+define('WEB_PATH', 'http://localhost/t.mandyok.com/');
+
+?>
